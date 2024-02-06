6b038da705917dc9ecdeb95117ff900608b40031
axepet <axepet@3R04MX3>
2024-02-06 15:06:27 +0100

    Add project files.

---
+++ Models/Court.cs
@@ -0,0 +1,28 @@
+﻿namespace MexicanoFrontend.Models
+{
+    using Mexicano.Mexicano;
+
+    public class Court
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string Name { get; set; } = string.Empty;
+
+        private List<Player> Players = new List<Player>();  
+
+        public List<Player> GetPlayers() 
+        {
+            return Players;
+        }    
+
+        public bool AddPlayersToCourt(Player p)
+        {
+            if(Players.Count < 4)
+            {
+                Players.Add(p);
+                return true;
+            }
+            else { return false; }
+            
+        }
+    }
+}
