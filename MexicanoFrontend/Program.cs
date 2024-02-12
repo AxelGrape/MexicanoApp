a17e74ea5367b812a8111f0125ba680b741e73f5
jwikstrom <jwikstrom56@gmail.com>
2024-02-12 11:51:49 +0100

    Added pointsPerGame with sessionstorage (#3)
    Co-authored-by: Joel Wikström <joel.wikstrom@softronic.se>

--- MexicanoFrontend/Program.cs
+++ MexicanoFrontend/Program.cs
@@ -3,6 +3,7 @@ using MexicanoFrontend;
 using MexicanoFrontend.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.SessionStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,5 +13,6 @@ builder.Services.AddBlazoredModal();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IRefreshService, RefreshService>();
+builder.Services.AddBlazoredSessionStorage();
 
 await builder.Build().RunAsync();
