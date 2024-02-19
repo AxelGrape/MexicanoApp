eace2eafc6b9602104231d1ab4677d091aed8869
Axel Pettersson <axel.pettersson.itg@gmail.com>
2024-02-19 09:16:44 +0100

    Added PlayerModel test

---
+++ MexicanoFrontent.Test/ModelTests/PlayerModelUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MexicanoFrontend.Models;
+using Mexicano.Mexicano;
+
+namespace MexicanoFrontent.Test.ModelTests
+{
+	public class PlayerModelUnitTest
+	{
+		[Fact]
+		public void CopyPlayerTest()
+		{
+			Player originalPlayer = new Player { Name = "Olle"};
+			Player copyPlayer = originalPlayer.CopyPlayer();
+
+
+			copyPlayer.Points = 10;
+
+			Assert.False(copyPlayer.Equals(originalPlayer));	
+		}
+	}
+}
